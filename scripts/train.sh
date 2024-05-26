b75434b104af2c5dc92d5a4aa8feb01b03594799
DimensionSTP <ddang8jh@gmail.com>
2024-05-27 08:06:37 +0900

    feat: basic scripts

---
+++ scripts/train.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+is_tuned="untuned"
+strategy="ddp"
+image_upload_user="microsoft"
+image_model_type="dit-large-finetuned-rvlcdip"
+text_upload_user="klue"
+text_model_type="roberta-large"
+precision=32
+batch_size=16
+
+python main.py --config-name=multimodal.yaml \
+    mode=train \
+    is_tuned=$is_tuned \
+    strategy=$strategy \
+    image_upload_user=$image_upload_user \
+    image_model_type=$image_model_type \
+    text_upload_user=$text_upload_user \
+    text_model_type=$text_model_type \
+    precision=$precision \
+    batch_size=$batch_size
+
+modality="text"
+is_tuned="untuned"
+strategy="ddp"
+upload_user="klue"
+model_type="roberta-large"
+precision=32
+batch_size=24
+
+python main.py --config-name=huggingface.yaml \
+    mode=train \
+    modality=$modality \
+    is_tuned=$is_tuned \
+    strategy=$strategy \
+    upload_user=$upload_user \
+    model_type=$model_type \
+    precision=$precision \
+    batch_size=$batch_size
+
+modality="image"
+is_tuned="untuned"
+strategy="ddp"
+upload_user="microsoft"
+model_type="dit-large-finetuned-rvlcdip"
+precision=32
+batch_size=24
+
+python main.py --config-name=huggingface.yaml \
+    mode=train \
+    modality=$modality \
+    is_tuned=$is_tuned \
+    strategy=$strategy \
+    upload_user=$upload_user \
+    model_type=$model_type \
+    precision=$precision \
+    batch_size=$batch_size
+
+modality="multimodal"
+is_tuned="untuned"
+strategy="ddp"
+upload_user="nielsr"
+model_type="layoutlmv3-finetuned-cord"
+precision=32
+batch_size=24
+
+python main.py --config-name=huggingface.yaml \
+    mode=train \
+    modality=$modality \
+    is_tuned=$is_tuned \
+    strategy=$strategy \
+    upload_user=$upload_user \
+    model_type=$model_type \
+    precision=$precision \
+    batch_size=$batch_size
+
+is_tuned="untuned"
+strategy="ddp"
+model_type="efficientnet_b0.ra_in1k"
+pretrained="pretrained"
+precision=32
+batch_size=24
+
+python main.py --config-name=timm.yaml \
+    mode=train \
+    is_tuned=$is_tuned \
+    strategy=$strategy \
+    model_type=$model_type \
+    pretrained=$pretrained \
+    precision=$precision \
+    batch_size=$batch_size
