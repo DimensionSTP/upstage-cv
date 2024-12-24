7e2dda6b10ef011da0b9bfc33cefced008cebb34
DimensionSTP <ddang8jh@gmail.com>
2024-12-24 18:36:07 +0900

    feat: default args

--- scripts/prepare_upload.sh
+++ scripts/prepare_upload.sh
@@ -3,22 +3,14 @@
 path="src/postprocessing"
 is_tuned="untuned"
 strategy="ddp"
-image_upload_user="microsoft"
-image_model_type="dit-large-finetuned-rvlcdip"
-text_upload_user="klue"
-text_model_type="roberta-large"
 precision=32
 batch_size=16
 epoch=10
-model_detail="roberta-large"
+model_detail="multimodal-transformer"
 
-python $path/prepare_upload.py --config-name=huggingface.yaml \
+python $path/prepare_upload.py --config-name=multimodal.yaml \
     is_tuned=$is_tuned \
     strategy=$strategy \
-    image_upload_user=$image_upload_user \
-    image_model_type=$image_model_type \
-    text_upload_user=$text_upload_user \
-    text_model_type=$text_model_type \
     precision=$precision \
     batch_size=$batch_size \
     epoch=$epoch \
